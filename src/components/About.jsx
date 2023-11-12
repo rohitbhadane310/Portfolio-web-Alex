7822c8448f58be35714bf58f7651b00a62fcd427
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-11-12 15:50:21 +0530

    working on the education section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,13 +9,13 @@ const About = () => {
   return (
     <div className=" min-h-screen flex flex-col justify-around lg:gap-8 lg:px-32 px-5 bg-backgroundColor">
       <div className=" mt-16">
-        <h1 className=" text-4xl font-semibold text-center lg:mt-4">
+        <h1 className=" text-4xl font-semibold text-center lg:mt-2">
           About Me
         </h1>
       </div>
 
       <div className=" -mt-10">
-        <div className=" mb-14">
+        <div className=" mb-10">
           <h2 className=" text-3xl font-semibold">I'm Alex Anderson</h2>
           <p className=" mt-4 lg:mt-2">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
--- src/layouts/ServicesCard.jsx
+++ src/layouts/ServicesCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ServicesCard = (props) => {
   return (
-    <div className=" flex flex-col items-center bg-white lg:w-1/4 py-4 gap-4 rounded-md">
+    <div className=" flex flex-col items-center bg-white hover:bg-black hover:text-white transition-all hover:-translate-y-2 w-full lg:w-1/4 py-5 gap-4 cursor-pointer rounded-md">
       <div>
         <span>{props.icon}</span>
       </div>
